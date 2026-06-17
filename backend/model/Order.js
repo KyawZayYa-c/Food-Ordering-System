e5fa4f97497af7dde1edef14dd2a208e1daa4feb
Kyaw Zay Ya <zayya4281@gmail.com>
2026-06-17 23:11:18 +0630

    database model is finish

---
+++ backend/model/Order.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    customer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    items: [{
+        product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+        quality: { type: Number, required: true }
+    }],
+    total_amount: { type: Number, required: true },
+    status: {
+        type: String,
+        enum: ['Pending', 'Preparing', 'Delivered', 'Cancelled'],
+        default: 'Pending'
+    },
+    payment_status: { type: String, default: 'Pending' },
+    payment_id: { type: String }
+}, {timestamps : true});
---
+++ backend/model/Product.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String },
+    price: { type: Number, required: true },
+    image_url: { type: String },
+    category: { type: String, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Product', productSchema);
